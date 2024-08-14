07cab9704ac3a990650fa7c84645f007fc27cb3e
egorReka <e.reka55@yandex.ru>
2024-08-14 20:48:53 +0300

    feat: add nav

--- source/js/button-nav.js
+++ source/js/button-nav.js
@@ -1,10 +1,12 @@
 const buttonNavElement = document.querySelector('.nav-button');
 const navContainer = document.querySelector('.header__container');
+const navElement = document.querySelector('.nav');
 
 
 export function initButtonNav() {
   buttonNavElement.addEventListener('click', () => {
     navContainer.classList.toggle('header__container--active');
     buttonNavElement.classList.toggle('nav-button--active');
+    navElement.classList.toggle('nav--active');
   });
 }
